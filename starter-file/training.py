eaf10812e72330dbdcc89596b49a3f154310ac5a
Alexander Kindle <alexander.a.kindle@accenture.com>
2026-03-09 20:31:31 +0100

    added model training

--- starter-file/training.py
+++ starter-file/training.py
@@ -9,24 +9,52 @@ from sklearn.linear_model import LogisticRegression
 import json
 
 ###################Load config.json and get path variables
-with open('config.json','r') as f:
+with open('./starter-file/config.json','r') as f:
     config = json.load(f) 
 
-dataset_csv_path = os.path.join(config['output_folder_path']) 
-model_path = os.path.join(config['output_model_path']) 
+
+dataset_csv_path = os.path.join(config['output_folder_path'], 'finaldata.csv') 
+model_path = os.path.join(config['output_model_path'], 'trainedmodel.pkl') 
 
 
 #################Function for training the model
 def train_model():
+
+    # reading data
+
+    df = pd.read_csv(dataset_csv_path)
+
+    # separate features and target
+
+    X = df.drop(['corporation', 'exited'], axis=1)
+    y = df['exited']
+
+
     
-    #use this logistic regression for training
-    LogisticRegression(C=1.0, class_weight=None, dual=False, fit_intercept=True,
-                    intercept_scaling=1, l1_ratio=None, max_iter=100,
-                    multi_class='warn', n_jobs=None, penalty='l2',
+    # use this logistic regression for training
+    model = LogisticRegression(C=1.0, class_weight=None, dual=False, fit_intercept=True,
+                    intercept_scaling=1, l1_ratio=None, max_iter=100, n_jobs=None, penalty='l2',
                     random_state=0, solver='liblinear', tol=0.0001, verbose=0,
                     warm_start=False)
     
-    #fit the logistic regression to your data
     
-    #write the trained model to your workspace in a file called trainedmodel.pkl
+    # fit model
+    model.fit(X, y)
+    
+    # write the trained model to your workspace in a file called trainedmodel.pkl
+
+    if not os.path.exists(config['output_model_path']):
+        os.makedirs(config['output_model_path'])
+
+
+
+    with open(model_path, 'wb') as f:
+        pickle.dump(model, f)
+
+
+    return model
+
+if __name__ == '__main__':
+    train_model()
+
 
