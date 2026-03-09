86f11d345da1378c96d6dac34383b153c2528993
Alexander Kindle <alexander.a.kindle@accenture.com>
2026-03-09 21:10:52 +0100

    addded deployment.py

--- starter-file/deployment.py
+++ starter-file/deployment.py
@@ -7,20 +7,37 @@ from sklearn import metrics
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 import json
-
+import shutil
 
 
 ##################Load config.json and correct path variable
-with open('config.json','r') as f:
+with open('./starter-file/config.json','r') as f:
     config = json.load(f) 
 
-dataset_csv_path = os.path.join(config['output_folder_path']) 
+model_path = os.path.join(config['output_model_path'], 'trainedmodel.pkl')
+score_path = os.path.join(config['output_model_path'], 'latestscore.txt')
+ingest_path = os.path.join(config['output_folder_path'], 'ingestedfiles.txt')
 prod_deployment_path = os.path.join(config['prod_deployment_path']) 
 
 
 ####################function for deployment
 def store_model_into_pickle(model):
     #copy the latest pickle file, the latestscore.txt value, and the ingestfiles.txt file into the deployment directory
+    
+    if not os.path.exists(prod_deployment_path):
+        os.makedirs(prod_deployment_path)
+
+    # copy model
+    shutil.copy(model_path, prod_deployment_path)
+
+    # copy score
+    shutil.copy(score_path, prod_deployment_path)
+
+    # copy ingest files
+    shutil.copy(ingest_path, prod_deployment_path)
+
+if __name__ == '__main__':
+    store_model_into_pickle('trainedmodel.pkl')
         
         
         
