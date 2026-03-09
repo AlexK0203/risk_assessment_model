a6d4ca5a31ef00b29f7c0d61773b8a9c327136c2
Alexander Kindle <alexander.a.kindle@accenture.com>
2026-03-09 22:02:08 +0100

    added diagnostics.py

--- starter-file/diagnostics.py
+++ starter-file/diagnostics.py
@@ -4,37 +4,103 @@ import numpy as np
 import timeit
 import os
 import json
+import pickle
+import subprocess
 
 ##################Load config.json and get environment variables
-with open('config.json','r') as f:
+with open('./starter-file/config.json','r') as f:
     config = json.load(f) 
-
-dataset_csv_path = os.path.join(config['output_folder_path']) 
-test_data_path = os.path.join(config['test_data_path']) 
 
 ##################Function to get model predictions
 def model_predictions():
     #read the deployed model and a test dataset, calculate predictions
-    return #return value should be a list containing all predictions
+
+    prod_model = os.path.join(config['prod_deployment_path'], 'trainedmodel.pkl') 
+    dataset_path = os.path.join(config['output_folder_path'], 'finaldata.csv')
+
+    with open(prod_model, 'rb') as f:
+        model = pickle.load(f)
+
+    df = pd.read_csv(dataset_path)
+
+    X = df.drop(['corporation', 'exited'], axis=1, errors='ignore')
+    predictions = model.predict(X)
+
+    return predictions.tolist() #return value should be a list containing all predictions
 
 ##################Function to get summary statistics
 def dataframe_summary():
+
+    dataset_path = os.path.join(config['output_folder_path'], 'finaldata.csv')
+            
+    # Read the data
+    df = pd.read_csv(dataset_path)
+    numeric_columns = df.select_dtypes(include='number')
+
     #calculate summary statistics here
-    return #return value should be a list containing all summary statistics
+
+    means = numeric_columns.mean()
+
+    medians = numeric_columns.median()
+    standard_deviation = numeric_columns.std()
+
+
+    # Convert each Series to a list
+    mean_list = means.tolist()
+    median_list = medians.tolist()
+    std_list = standard_deviation.tolist()
+
+    # Combine them into one single list
+    summary_list = mean_list + median_list + std_list
+
+    return summary_list #return value should be a list containing all summary statistics
+
+##################Function count missing data
+def missing_data():
+    #  Load the data
+    dataset_path = os.path.join(config['output_folder_path'], 'finaldata.csv')
+    df = pd.read_csv(dataset_path)
+    
+    # Calculate percentages of NA values per column
+    # nas_count / total_rows * 100
+    nas_percentages = (df.isna().sum() / len(df)) * 100
+    
+    # Return as a list
+    return nas_percentages.tolist()
+
 
 ##################Function to get timings
 def execution_time():
     #calculate timing of training.py and ingestion.py
-    return #return a list of 2 timing values in seconds
+    #  Time the Ingestion script
+    start_ingest = timeit.default_timer()
+    os.system('python3 ./starter-file/ingestion.py')
+    end_ingest = timeit.default_timer()
+    ingestion_time = end_ingest - start_ingest
+
+    # Time the Training script
+    start_train = timeit.default_timer()
+    os.system('python3 ./starter-file/training.py')
+    end_train = timeit.default_timer()
+    training_time = end_train - start_train
+
+    return [ingestion_time, training_time]
+
 
 ##################Function to check dependencies
 def outdated_packages_list():
-    #get a list of 
+    #get a list of outdated packages
+    result = subprocess.run(['pip', 'list', '--outdated'], 
+                            capture_output=True, 
+                            text=True)
+    print(result.stdout)
+    return result.stdout
 
 
 if __name__ == '__main__':
     model_predictions()
     dataframe_summary()
+    missing_data()
     execution_time()
     outdated_packages_list()
 
